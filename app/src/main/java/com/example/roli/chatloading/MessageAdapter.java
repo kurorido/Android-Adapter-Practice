8d8467b6b8e7599f6fdc444371913bca87a3007f
roli <kuroridoplayer@gmail.com>
2016-06-05 22:21:03 +0800

    add animation

--- app/src/main/java/com/example/roli/chatloading/MessageAdapter.java
+++ app/src/main/java/com/example/roli/chatloading/MessageAdapter.java
@@ -5,6 +5,8 @@ import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -63,10 +65,16 @@ public class MessageAdapter extends BaseAdapter {
         if(msg.getType() == Message.MessageType.LOADING) {
             holder.text.setVisibility(View.GONE);
             holder.image.setVisibility(View.VISIBLE);
+            AlphaAnimation anim = new AlphaAnimation(0.2f, 1.0f);
+            anim.setDuration(1000);
+            anim.setRepeatMode(Animation.REVERSE);
+            anim.setRepeatCount(Animation.INFINITE);
+            holder.image.startAnimation(anim);
         } else {
             holder.text.setVisibility(View.VISIBLE);
             holder.image.setVisibility(View.GONE);
             holder.text.setText(msg.getText());
+            holder.image.setAnimation(null);
         }
 
         return convertView;
